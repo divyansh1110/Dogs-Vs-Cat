0948cfadb3526a0bc1643706b493f732044b5475
divyansh1110 <divyanshchawda1@gmail.com>
2021-07-19 11:11:51 +0530

    added print statement for info about files deleted

--- check data.py
+++ check data.py
@@ -1,24 +1,26 @@
 import numpy as np
 import cv2
 import os
-from tqdm import tqdm
-
 
 path='Data/Petimages'
 subFolders=['Cat','Dog']
-i=0
+
+print('Total no of files before removing corrupted files')
+print(len(os.listdir('Data/Petimages/Cat'))+len(os.listdir('Data/Petimages/Dog')))
+
+corrupted=0
 for currentFolder in subFolders:
     currentPath=os.path.join(path,currentFolder)
     for img in os.listdir(currentPath):
         try:
-            img_array = cv2.imread(os.path.join(currentPath,img)  ,cv2.IMREAD_GRAYSCALE)  # convert to array
-            new_array = cv2.resize(img_array, (100, 100))  # resize to normalize data size
-        except Exception as e:  # in the interest in keeping the output clean...
+            img_array = cv2.imread(os.path.join(currentPath,img)  ,cv2.IMREAD_GRAYSCALE)  
+            new_array = cv2.resize(img_array, (100, 100))  
+        except Exception as e:  
                 print(os.path.join(currentPath,img))
-                i=i+1
+                corrupted=corrupted+1
                 os.remove(os.path.join(currentPath,img))
 
-print(i)  
+print("Total no of corrupted files {}".format(corrupted))  
+print('Total no of files After removing corrupted files')
+print(len(os.listdir('Data/Petimages/Cat'))+len(os.listdir('Data/Petimages/Dog')))
         
-
-    
